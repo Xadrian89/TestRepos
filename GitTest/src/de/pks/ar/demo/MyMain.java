34e338507a615a2b73ddd2fed4a0a040f5d9abe1
adrian <reichart@pks.de>
2018-11-09 10:27:18 +0100

    compliment to the world

--- GitTest/src/de/pks/ar/demo/MyMain.java
+++ GitTest/src/de/pks/ar/demo/MyMain.java
@@ -3,7 +3,7 @@ package de.pks.ar.demo;
 public class MyMain {
 
 	public static void main(String[] args) {
-		String anfrage = "Hello World!";
+		String anfrage = "Hello World! I like you!";
 		System.out.println(anfrage);
 		World.antworte(anfrage);
 		}
